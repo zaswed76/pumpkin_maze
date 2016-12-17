51a22736fcee204eae4aacd25a3ba2a4409f1ecc
Admin <zaswed76@gmail.com>
2016-12-18 00:03:30 +0200

    gui ...

--- pumpkin/gui/widgets.py
+++ pumpkin/gui/widgets.py
@@ -2,6 +2,8 @@
 
 import sys
 import os
+from functools import partial
+
 from PyQt5 import QtWidgets, QtCore, QtGui
 from pumpkin import paths
 
@@ -11,9 +13,10 @@ class ArrowButton(QtWidgets.QPushButton):
         super().__init__(*__args)
         self.setCheckable(True)
         self.setFlat(True)
-        size = QtCore.QSize(*kwargs['size_icon'])
-        self.setFixedSize(size)
-        self.setIconSize(size)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Expanding,
+            QtWidgets.QSizePolicy.Expanding)
+        self.setSizePolicy(sizePolicy)
 
 
 class Label(QtWidgets.QLabel):
@@ -23,16 +26,25 @@ class Label(QtWidgets.QLabel):
 
 class InputSide(QtWidgets.QFrame):
     ext_icon = '.png'
+    size_reduction = 10
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.grid = QtWidgets.QGridLayout(self)
-        self.grid.setSpacing(1)
+        self.box = QtWidgets.QVBoxLayout(self)
+        self.box.addWidget(Label('Input sides'))
+        self.grid = QtWidgets.QGridLayout()
+        self.box.addLayout(self.grid)
+
+        self.grid.setSpacing(0)
         self.grid.setContentsMargins(0, 0, 0, 0)
 
         self.size_icon = (32, 32)
         self.buttons_list = ['plus', 'top', None, 'left', 'door',
                              'right', None, 'bottom', 'minus']
+        self.buttons = {}
         self.add_widgets()
+        size = self.size_icon[0] * 3 + (4 * 13)
+        self.setFixedSize(size, size)
 
     def add_widgets(self):
         n = 0
@@ -40,11 +52,26 @@ class InputSide(QtWidgets.QFrame):
             for y in range(3):
                 button_name = self.buttons_list[n]
                 if button_name is not None:
-                    direct = str(self.size_icon[0])
-                    pth = paths.icon_path(direct, button_name, self.ext_icon)
-                    icon = QtGui.QIcon(pth)
-                    self.grid.addWidget(ArrowButton(icon, "", size_icon=self.size_icon), x, y)
+                    self.buttons[button_name] = ArrowButton()
+                    self.buttons[button_name].clicked.connect(
+                        partial(self.button_press, button_name))
+                    self.buttons[button_name].setObjectName(
+                        button_name)
+                    size = QtCore.QSize(*self.size_icon)
+                    self.buttons[button_name].setFixedSize(size)
+                    self.buttons[button_name].setIconSize(size)
+                    self.grid.addWidget(self.buttons[button_name], x,
+                                        y)
                 n += 1
+        mini_size = self.size_icon[0] - self.size_reduction
+        qsize = QtCore.QSize(mini_size, mini_size)
+        self.buttons['plus'].setFixedSize(qsize)
+        self.buttons['minus'].setFixedSize(qsize)
+        self.buttons['plus'].setIconSize(qsize)
+        self.buttons['minus'].setIconSize(qsize)
+
+    def button_press(self, name):
+        print('button_press', name)
 
 
 class Widget(QtWidgets.QFrame):
@@ -53,6 +80,7 @@ class Widget(QtWidgets.QFrame):
         self.resize(300, 200)
         self.setWindowTitle('Tiled gui lib')
         self.box = QtWidgets.QHBoxLayout(self)
+
         self.box.setContentsMargins(1, 1, 1, 1)
 
     def set_widget(self, widget):
@@ -64,6 +92,8 @@ class PortalDialog(QtWidgets.QFrame):
         super().__init__(*args, **kwargs)
 
         self.box = QtWidgets.QVBoxLayout(self)
+        self.box.setContentsMargins(0, 0, 0, 0)
+        self.box.setSpacing(0)
         self.top_widget = QtWidgets.QFrame()
         self.side_widget = InputSide()
         self.box.addWidget(self.top_widget)
@@ -72,6 +102,7 @@ class PortalDialog(QtWidgets.QFrame):
         self.parent.setWindowTitle('Set Portal')
 
         self.check_portal = QtWidgets.QCheckBox()
+        self.check_portal.setFixedSize(32, 32)
         # откуда
 
         self.start_id = QtWidgets.QLineEdit()
@@ -81,15 +112,13 @@ class PortalDialog(QtWidgets.QFrame):
 
         self.form = QtWidgets.QFormLayout(self.top_widget)
 
-        self.form.setSpacing(12)
-        self.form.addRow(Label('Portal'), self.check_portal)
+        self.form.setSpacing(20)
+        label_portal = Label('Portal')
+        label_portal.setMinimumWidth(40)
+        self.form.addRow(label_portal, self.check_portal)
         # self.form.addRow(Label('ID входа'), self.start_id)
 
-    def input_side(self):
-        self.form.addRow(Label('вход слева'), self.start_id)
-        self.form.addRow(Label('вход сверху'), self.start_id)
-        self.form.addRow(Label('вход справа'), self.start_id)
-        self.form.addRow(Label('вход сснизу'), self.start_id)
+
 
 
 portal = PortalDialog
